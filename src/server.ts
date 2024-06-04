bd2679e7ba7fd3e531edbec81b5d4953ba7e74be
Bruno Henrique <brunodorea@outlook.com.br>
2024-06-04 16:11:28 -0300

    :sparkles: feat: configurando o express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', function (req: Request, res: Response) {
+  res.status(200).json({player: "Beckham"})
+})
+
+app.listen(port, () => {
+  console.log(`🔥 Server running at port http://localhost:${port}`)
+})
